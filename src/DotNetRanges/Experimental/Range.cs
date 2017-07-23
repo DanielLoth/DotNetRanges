285b6131426ecdf6fdea0db8869bd298762b05a6
Daniel Loth <lothyis@gmail.com>
2017-07-23 13:54:46 +1000

    Added initial project with Experimental namespace (this entire project is not intended to be production-grade at this point in time).
    Added test project.

---
+++ src/DotNetRanges/Experimental/Range.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DotNetRanges.Experimental
+{
+    public struct Range<T> : IEquatable<Range<T>>
+        where T : IComparable<T>, IEquatable<T>
+    {
+        #region IEquatable<T>
+
+        public bool Equals(Range<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Object overrides
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ test/DotNetRanges.Tests/Experimental/RangeTests.cs
@@ -0,0 +1,17 @@
+﻿using DotNetRanges.Experimental;
+using Xunit;
+
+namespace DotNetRanges.Tests.Experimental
+{
+    public class RangeTests
+    {
+        [Fact]
+        public void FirstUnitTest()
+        {
+            var range = new Range<int>();
+
+            Assert.NotNull(range);
+            Assert.Equal(1, 1);
+        }
+    }
+}
