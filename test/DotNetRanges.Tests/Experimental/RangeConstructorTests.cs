721d50c9a0c9283905388cf8c3771f3abeb3fcbb
Daniel Loth <lothyis@gmail.com>
2017-07-29 22:55:37 +1000

    Improved constructor test coverage.

--- test/DotNetRanges.Tests/Experimental/RangeConstructorTests.cs
+++ test/DotNetRanges.Tests/Experimental/RangeConstructorTests.cs
@@ -16,5 +16,38 @@ namespace DotNetRanges.Tests.Experimental
         {
             Assert.Throws<InvalidOperationException>(() => Range<T>.Open(endpoint, endpoint));
         }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData("a")]
+        [InlineData('a')]
+        [InlineData(5.9f)]
+        public void AllowConstructionOfSingletonRange<T>(T endpoint)
+            where T : IComparable<T>
+        {
+            var range = Range<T>.Closed(endpoint, endpoint);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData("a")]
+        [InlineData('a')]
+        [InlineData(5.9f)]
+        public void AllowConstructionOfEmptyClosedOpenRange<T>(T endpoint)
+            where T : IComparable<T>
+        {
+            var range = Range<T>.ClosedOpen(endpoint, endpoint);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData("a")]
+        [InlineData('a')]
+        [InlineData(5.9f)]
+        public void AllowConstructionOfEmptyOpenClosedRange<T>(T endpoint)
+            where T : IComparable<T>
+        {
+            var range = Range<T>.OpenClosed(endpoint, endpoint);
+        }
     }
 }
